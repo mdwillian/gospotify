e5c4a532f2bc34a9d8f00eb073174a2ab7b2ac47
Willian Domingos <mdwillian@gmail.com>
2019-07-14 19:41:17 -0300

    Criando componente player

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 
 import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+
+import { Wrapper, Container } from './styles/components';
 
 function App() {
   return (
-    <fragment>
+    <Wrapper>
       <GlobalStyle />
-      <Sidebar />
-    </fragment>
+      <Container>
+        <Sidebar />
+      </Container>
+      <Player />
+    </Wrapper>
   );
 }
 
