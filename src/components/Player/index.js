1e0443b7be80a01f478306e4e88339066301f336
Willian Domingos <mdwillian@gmail.com>
2019-08-03 22:42:01 -0300

    Slider de duração

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -20,11 +20,16 @@ import ForwardIcon from '../../assets/images/forward.svg';
 import RepeatIcon from '../../assets/images/repeat.svg';
 
 const Player = ({
-  player, play, pause, prev, next,
+  player, position, duration, play, pause, prev, next, playing,
 }) => (
   <Container>
     {!!player.currentSong && (
-      <Sound url={player.currentSong.file} playStatus={player.status} onFinishedPlaying={next} />
+      <Sound
+        url={player.currentSong.file}
+        playStatus={player.status}
+        onFinishedPlaying={next}
+        onPlaying={playing}
+      />
     )}
 
     <Current>
@@ -66,7 +71,7 @@ const Player = ({
       </Controls>
 
       <Time>
-        <span>1:39</span>
+        <span>{position}</span>
         <ProgressSlider>
           <Slider
             railStyle={{ background: '#404040', borderRadius: 10 }}
@@ -74,7 +79,7 @@ const Player = ({
             handleStyle={{ border: 0 }}
           />
         </ProgressSlider>
-        <span>4:24</span>
+        <span>{duration}</span>
       </Time>
     </Progress>
 
@@ -100,14 +105,28 @@ Player.propTypes = {
     }),
     status: PropTypes.string,
   }).isRequired,
+  position: PropTypes.string.isRequired,
+  duration: PropTypes.string.isRequired,
   play: PropTypes.func.isRequired,
   pause: PropTypes.func.isRequired,
   prev: PropTypes.func.isRequired,
   next: PropTypes.func.isRequired,
+  playing: PropTypes.func.isRequired,
 };
+
+function msToTime(duration) {
+  let seconds = parseInt((duration / 1000) % 60, 10);
+  const minutes = parseInt((duration / (1000 * 60)) % 60, 10);
+
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return `${minutes}:${seconds}`;
+}
 
 const mapStateToProps = state => ({
   player: state.player,
+  position: msToTime(state.player.position),
+  duration: msToTime(state.player.duration),
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(PlayerActions, dispatch);
